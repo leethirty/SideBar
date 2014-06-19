20dc058a381fdf53f8f9758d6e637116c275aa06
leethirty <leerjgc@gmail.com>
2014-06-19 19:45:28 +0800

    Initial Commit

---
+++ SideBarDemon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SideBarDemon
+//
+//  Created by Lee on 14-6-19.
+//  Copyright (c) 2014年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
